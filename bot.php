a8cd69d94e170fb903ab7b5d6d1595643e86f03c
tunzzz <assadayut.pt@hotmail.com>
2017-06-06 22:19:52 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -19,7 +19,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text
+				'text' => 'test1234'
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
